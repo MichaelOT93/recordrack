4e7889ed3546e4073872ef05df83654f87709d3e
Michael O'Toole <michaelotoole93@gmail.com>
2020-11-28 15:09:11 +0000

    implemented User CRUD features

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,17 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+
     def remember(user)
         user.remember
         cookies.permanent.signed[:user_id] = user.id
             cookies.permanent[:remember_token] = user.remember_token
     end
 
+
+    def current_user?(user)
+        user == current_user
+    end
 
 
     def current_user 
@@ -41,6 +46,21 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
+
+
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location 
+        session[:forwarding_url] = request.original_url 
+        if request.get?
+    end
+end
+
+    
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get '/users' => 'users#index'
 
+  
+
 
 
 
