323f17325be82af79f478401ee399e6cdab95225
Michael O'Toole <michaelotoole93@gmail.com>
2020-11-28 12:41:13 +0000

    implemented more advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_195243) do
+ActiveRecord::Schema.define(version: 2020_11_28_114046) do
 
   create_table "records", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_195243) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
